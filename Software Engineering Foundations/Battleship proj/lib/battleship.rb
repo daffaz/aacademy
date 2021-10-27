1a3c15d78b5b9fb6555ed34645f77489ed913d9b
Daffa Z <67418594+daffaz@users.noreply.github.com>
2021-10-27 17:36:59 +0700

    battleship solution

--- Software Engineering Foundations/Battleship proj/lib/battleship.rb
+++ Software Engineering Foundations/Battleship proj/lib/battleship.rb
@@ -2,5 +2,48 @@ require_relative "board"
 require_relative "player"
 
 class Battleship
+  attr_reader :player, :board, :remaining_misses
+  # @param [Integer] n
+  def initialize(n)
+    @player = Player.new
+    @board = Board.new(n)
+    @remaining_misses = board.size / 2
+  end
 
+  def start_game
+    board.place_random_ships
+    p "there is #{board.num_ships} ships on board"
+    board.print
+  end
+
+  def lose?
+    if remaining_misses <= 0
+      p 'you lose'
+      true
+    else
+      false
+    end
+  end
+
+  def win?
+    if board.num_ships.positive?
+      false
+    else
+      p 'you win'
+      true
+    end
+  end
+
+  def game_over?
+    win? || lose? ? true : false
+  end
+
+  def turn
+    pos = player.get_move
+    attack = board.attack(pos)
+
+    @remaining_misses -= 1 unless attack
+    p board.print
+    p remaining_misses
+  end
 end
