5cfc38a13741b4067da16963d48273d04b5a9962
Daffa Z <67418594+daffaz@users.noreply.github.com>
2021-10-30 18:38:00 +0700

    70% complete

---
+++ Software Engineering Foundations/Perilous Procs/solution.rb
@@ -0,0 +1,198 @@
+# @param [Array] arr
+# @param [Proc] prc
+def some?(arr, &prc)
+  arr.each { |item| return true if prc.call(item) }
+  false
+end
+
+# p some?([3, 1, 11, 5]) { |n| n.even? }                                # false
+# p some?([3, 4, 11, 5]) { |n| n.even? }                                # true
+# p some?([8, 2]) { |n| n.even? }                                       # true
+# p some?(['squash', 'corn', 'kale', 'carrot']) { |str| str[0] == 'p' } # false
+# p some?(['squash', 'corn', 'kale', 'potato']) { |str| str[0] == 'p' } # true
+# p some?(['parsnip', 'lettuce', 'pea']) { |str| str[0] == 'p' }        # true
+
+# @param [Array] arr
+# @param [Proc] prc
+# @param [Integer] number
+def exactly?(arr, number, &prc)
+  count = 0
+  arr.each { |item| count += 1 if prc.call(item) }
+  count == number
+end
+
+# p exactly?(['A', 'b', 'C'], 2) { |el| el == el.upcase }         # true
+# p exactly?(['A', 'B', 'C'], 2) { |el| el == el.upcase }         # false
+# p exactly?(['A', 'B', 'C'], 3) { |el| el == el.upcase }         # true
+# p exactly?(['cat', 'DOG', 'bird'], 1) { |el| el == el.upcase }  # true
+# p exactly?(['cat', 'DOG', 'bird'], 0) { |el| el == el.upcase }  # false
+# p exactly?(['cat', 'dog', 'bird'], 0) { |el| el == el.upcase }  # true
+# p exactly?([4, 5], 3) { |n| n > 0 }                             # false
+# p exactly?([4, 5, 2], 3) { |n| n > 0 }                          # true
+# p exactly?([42, -9, 7, -3, -6], 2) { |n| n > 0 }                # true
+
+
+# @param [Array] arr
+# @param [Proc] prc
+def filter_out(arr, &prc)
+  filtered = []
+  arr.each { |item| filtered << item unless prc.call(item) }
+  filtered
+end
+
+# p filter_out([10, 6, 3, 2, 5 ]) { |x| x.odd? }      # [10, 6, 2]
+# p filter_out([1, 7, 3, 5 ]) { |x| x.odd? }          # []
+# p filter_out([10, 6, 3, 2, 5 ]) { |x| x.even? }     # [3, 5]
+# p filter_out([1, 7, 3, 5 ]) { |x| x.even? }         # [1, 7, 3, 5]
+
+
+# @param [Array] arr
+# @param [Proc] prc
+# @param [Integer] number
+def at_least?(arr, number, &prc)
+  count = 0
+  arr.each do |item|
+    count += 1 if prc.call(item)
+  end
+  count >= number
+end
+
+# p at_least?(['sad', 'quick', 'timid', 'final'], 2) { |s| s.end_with?('ly') }
+# # false
+# p at_least?(['sad', 'quickly', 'timid', 'final'], 2) { |s| s.end_with?('ly') }
+# # false
+# p at_least?(['sad', 'quickly', 'timidly', 'final'], 2) { |s| s.end_with?('ly') }
+# # true
+# p at_least?(['sad', 'quickly', 'timidly', 'finally'], 2) { |s| s.end_with?('ly') }
+# # true
+# p at_least?(['sad', 'quickly', 'timid', 'final'], 1) { |s| s.end_with?('ly') }
+# # true
+# p at_least?(['sad', 'quick', 'timid', 'final'], 1) { |s| s.end_with?('ly') }
+# # false
+# p at_least?([false, false, false], 3) { |bool| bool }
+# # false
+# p at_least?([false, true, true], 3) { |bool| bool }
+# # false
+# p at_least?([true, true, true], 3) { |bool| bool }
+# # true
+# p at_least?([true, true, true, true], 3) { |bool| bool }
+# # true
+
+def every?(arr, &prc)
+  arr.each { |item| return false unless prc.call(item) }
+  true
+end
+
+# p every?([3, 1, 11, 5]) { |n| n.even? }                                 # false
+# p every?([2, 4, 4, 8]) { |n| n.even? }                                  # true
+# p every?([8, 2]) { |n| n.even? }                                        # true
+# p every?(['squash', 'corn', 'kale', 'carrot']) { |str| str[0] == 'p' }  # false
+# p every?(['squash', 'pea', 'kale', 'potato']) { |str| str[0] == 'p' }   # false
+# p every?(['parsnip', 'potato', 'pea']) { |str| str[0] == 'p' }          # true
+
+
+def at_most?(arr, number, &prc)
+  count = 0
+  arr.each { |item| count += 1 if prc.call(item) }
+  number >= count
+end
+
+# p at_most?([-4, 100, -3], 1) { |el| el > 0 }                         # true
+# p at_most?([-4, -100, -3], 1) { |el| el > 0 }                        # true
+# p at_most?([4, 100, -3], 1) { |el| el > 0 }                          # false
+# p at_most?([4, 100, 3], 1) { |el| el > 0 }                           # false
+# p at_most?(['r', 'q', 'e', 'z'], 2) { |el| 'aeiou'.include?(el) }    # true
+# p at_most?(['r', 'i', 'e', 'z'], 2) { |el| 'aeiou'.include?(el) }    # true
+# p at_most?(['r', 'i', 'e', 'o'], 2) { |el| 'aeiou'.include?(el) }    # false
+
+def first_index(arr, &prc)
+  arr.each { |item| return arr.index(item) if prc.call(item) }
+  nil
+end
+
+# p first_index(['bit', 'cat', 'byte', 'below']) { |el| el.length > 3 }           # 2
+# p first_index(['bitten', 'bit', 'cat', 'byte', 'below']) { |el| el.length > 3 } # 0
+# p first_index(['bitten', 'bit', 'cat', 'byte', 'below']) { |el| el.length > 6 } # nil
+# p first_index(['bit', 'cat', 'byte', 'below']) { |el| el[0] == 'b' }            # 0
+# p first_index(['bit', 'cat', 'byte', 'below']) { |el| el.include?('a') }        # 1
+# p first_index(['bit', 'cat', 'byte', 'below']) { |el| el[0] == 't' }            # nil
+
+# @param [Array] arr
+# @param [Proc] prc1
+# @param [Proc] prc2
+def xnor_select(arr, prc1, prc2)
+  arr.select { |item| prc1.call(item) && prc2.call(item) || !(prc1.call(item) || prc2.call(item)) }
+  # arr.select { |item| prc1.call(item) == prc2.call(item) }
+end
+
+# is_even = Proc.new { |n| n % 2 == 0 }
+# is_odd = Proc.new { |n| n % 2 != 0 }
+# is_positive = Proc.new { |n| n > 0 }
+# p xnor_select([8, 3, -4, -5], is_even, is_positive)         # [8, -5]
+# p xnor_select([-7, -13, 12, 5, -10], is_even, is_positive)  # [-7, -13, 12]
+# p xnor_select([-7, -13, 12, 5, -10], is_odd, is_positive)   # [5, -10]
+
+# @param [Array] arr
+# @param [Proc] prc
+def filter_out!(arr, &prc)
+  arr.each { |item| arr.delete(item) if prc.call(item) }
+end
+
+# arr_1 = [10, 6, 3, 2, 5 ]
+# p arr_1.object_id
+# filter_out!(arr_1) { |x| x.odd? }
+# p arr_1     # [10, 6, 2]
+# p arr_1.object_id
+#
+# arr_2 = [1, 7, 3, 5 ]
+# filter_out!(arr_2) { |x| x.odd? }
+# p arr_2     # []
+#
+# arr_3 = [10, 6, 3, 2, 5 ]
+# filter_out!(arr_3) { |x| x.even? }
+# p arr_3     # [3, 5]
+#
+# arr_4 = [1, 7, 3, 5 ]
+# filter_out!([1, 7, 3, 5 ]) { |x| x.even? }
+# p arr_4 # [1, 7, 3, 5]
+
+
+def multi_map(arr, num = 1, &prc)
+  result = []
+  arr.each do |item|
+    num.times { item = prc.call(item) }
+    result << item
+  end
+  result
+end
+
+# p multi_map(['pretty', 'cool', 'huh?']) { |s| s + '!'}      # ["pretty!", "cool!", "huh?!"]
+# p multi_map(['pretty', 'cool', 'huh?'], 1) { |s| s + '!'}   # ["pretty!", "cool!", "huh?!"]
+# p multi_map(['pretty', 'cool', 'huh?'], 3) { |s| s + '!'}   # ["pretty!!!", "cool!!!", "huh?!!!"]
+# p multi_map([4, 3, 2, 7], 1) { |num| num * 10 }             # [40, 30, 20, 70]
+# p multi_map([4, 3, 2, 7], 2) { |num| num * 10 }             # [400, 300, 200, 700]
+# p multi_map([4, 3, 2, 7], 4) { |num| num * 10 }             #
+
+def proctition(arr, &prc)
+  trues = []
+  falses = []
+  arr.each do |item|
+    trues << item if prc.call(item)
+    falses << item unless prc.call(item)
+  end
+  [*trues, *falses]
+end
+
+# p proctition([4, -5, 7, -10, -2, 1, 3]) { |el| el > 0 }
+# # [4, 7, 1, 3, -5, -10, -2]
+#
+# p proctition([7, 8, 3, 6, 10]) { |el| el.even? }
+# # [8, 6, 10, 7, 3]
+#
+# p proctition(['cat','boot', 'dog', 'bug', 'boat']) { |s| s[0] == 'b' }
+# # ["boot", "bug", "boat", "cat", "dog"]
+
+def selected_map!(arr, prc1, prc2)
+  
+  return nil
+end
