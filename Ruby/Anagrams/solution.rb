d52fdf801c6d0110e04d25a0eee586acd20eba41
Daffa Z <67418594+daffaz@users.noreply.github.com>
2021-11-10 16:10:54 +0700

    phase 3

--- Ruby/Anagrams/solution.rb
+++ Ruby/Anagrams/solution.rb
@@ -2,19 +2,13 @@
 # @param [String] str2
 
 def first_anagram(_str1, str2)
-  result.include?(str2)
+  all_anagrams(str1).include?(str2)
 end
 
 def all_anagrams(str)
   return [str] if str.length <= 1
 
   prev_anagrams = all_anagrams(str[0...-1])
-  # "gizmo"
-  # all(gizmo)
-  # all(gizm)
-  # all(giz) => prev = [ig, gi] => [zig, izg, igz, zgi, gzi, giz]
-  # all(gi) => prev = [g] => [ig, gi]
-  # all(g) => [g]
   new_anagrams = []
 
   prev_anagrams.each do |anagram|
@@ -26,6 +20,28 @@ def all_anagrams(str)
   new_anagrams
 end
 
-p all_anagrams('g')
-p all_anagrams('gi')
-p all_anagrams('giz')
+# @param [String] str1
+# @param [String] str2
+def second_anagram(str1, str2)
+  arr1 = str1.split('')
+  arr2 = str2.split('')
+
+  arr1.each do |char|
+    target_idx = arr2.find_index(char)
+    return false unless target_idx
+
+    arr2.delete_at(target_idx)
+  end
+
+  arr2.empty?
+end
+
+def third_anagram(str1, str2)
+  sorted1 = str1.split('').sort
+  sorted2 = str2.split('').sort
+
+  sorted1 == sorted2
+end
+
+p third_anagram('gizmo', 'sally')    #=> false
+p third_anagram('elvis', 'lives')    #=> true
